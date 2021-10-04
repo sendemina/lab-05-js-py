64a2c3a4c5c21bccb8025e294a5df1d2c3fd301e
Kseniia Demina <Kseniia@kseniia-c02wv1hlhtdd.lan>
2021-10-04 16:12:41 -0400

    finished greetings in both py and js

---
+++ greetings.py
@@ -0,0 +1,8 @@
+username = input("Hi, what is your name? ")
+  # username is a symbol representing a placeholder in memory (VARIABLE)
+  # input() is a FUNCTION that collects user input
+  # the output of the input() function is ASSIGNED to the variable username
+print("Hi, " + str(username) + "!")
+  # print() is a PRINT function in Python
+  # str() is a function in Python that converts its input into a STRING
+  # + is a symbol for string concatenation
