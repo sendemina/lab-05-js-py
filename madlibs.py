e85f98485b08b693e926fec5f1a7b951f9f774a7
Sen <senjademina@gmail.com>
2021-10-04 16:53:04 -0400

    python version of madlibs is done

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = input("Pick a noun... ")
+adjective = input("How about an adjective? ")
+adverb = input("Now, an adverb... ")
+verb = input("A verb? Last thing, I swear! ")
+
+print("On a tropical " + str(noun) + ",")
+print("Underneath a " + str(adjective) + " lava moon.")
+print("Hangin' " + str(adverb) + " with the hulu dancers,")
+print(str(verb) + " questions 'cause they all got the answers")
